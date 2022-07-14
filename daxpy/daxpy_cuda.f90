af6a0cddd6ad9b7f9fcdabf35ea147e60b43730d
keroro90 <keroro.90@hotmail.it>
2022-07-14 16:54:05 +0200

    cuda_fix

--- daxpy/daxpy_cuda.f90
+++ daxpy/daxpy_cuda.f90
@@ -6,7 +6,7 @@ contains
   attributes(value) :: a,n
   n = size(x)
   i = blockDim%x * (blockIdx%x - 1) + threadIdx%x
-  if (i <= n) y(i) = y(i) + a*x(i)*x(i)*x(i) + sqrt(x(i))
+  if (i <= n) y(i) = y(i) + a*x(i)*x(i)
 end subroutine daxpy
 end module mymod
 
@@ -16,17 +16,24 @@ use cudafor
 use mymod
 integer ::n=50000000,i,istat
 double precision :: a
+double precision, allocatable :: x(:),y(:)
 double precision, device :: x_d(n), y_d(n)
 double precision :: t1,t2
 
-x_d=1.0
-y_d=2.0
-a=3.0d0
+allocate(x(n),y(n))
+
+x=1.0d0
+y=2.0d0
+a=3.d0
 
 call cpu_time(t1)
-call daxpy<<<4096, 256>>>(2**25,a,x_d,y_d)
-istat = cudaDeviceSynchronize()
+x_d=x
+y_d=y
+call daxpy<<<256, 256>>>(n,a,x_d,y_d)
+y=y_d
 call cpu_time(t2)
 write(*,*) 'Time elapsed (CUDA):', t2-t1
+
+deallocate(x,y)
 
 end program
